b7e67bed9fd839c2425e8845a3a3f28e037f2ef8
전수열 <devxoul@gmail.com>
2013-01-11 22:20:53 +0900

    Initial Commit

---
+++ JLFoldableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JLFoldableView
+//
+//  Created by 전수열 on 13. 1. 11..
+//  Copyright (c) 2013년 Joyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JLFoldableView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JLFoldableView
+//
+//  Created by 전수열 on 13. 1. 11..
+//  Copyright (c) 2013년 Joyfl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
